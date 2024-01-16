95f3c60cce40d77147939f7cd19cb6f305f0f4c4
Matthew Kwon <matthew.o.kwon@gmail.com>
2024-01-15 16:05:04 -0800

    Created main app file and implemented custom exception handling for accounts and messages

---
+++ src/main/java/SocialMediaApp.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class SocialMediaApp {
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(SocialMediaApp.class, args);
+    }
+}
